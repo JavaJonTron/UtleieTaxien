78060e77758146fe0f057d307fa0006279cc6066
Ole4961 <marius.torvmark@gmail.com>
2022-09-26 23:40:23 +0200

    Ryddet opp i kode og kommentarer

--- classes/car.py
+++ classes/car.py
@@ -6,7 +6,3 @@ class Car:
         self.make = make
         self.km = km
         self.year = year
-
-    def car_info(self):
-        return "Car is a: " + self.make + " " + self.model + "\nModel Year: " + self.year + "\nFuel type: " + \
-               self.fuel_source + "\nOdometer: " + self.km + "\nRegistration number: " + self.regnumber
--- main.py
+++ main.py
@@ -1,31 +1,5 @@
-from classes import owner
-from classes import car
-from classes import renter
 from classes.car import Car
 
-
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    # print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    # commit -test, fra Jon
-    # Press the green button in the gutter to run the script.
-    if __name__ == '__main__':
-        print_hi('PyCharm')
-
-
-# ownerCar1 = Car("ED12345", "Electric", "Model Y", "Tesla", "2022", "6332")
-
-# print(ownerCar1.car_info())
-
-# /*cars = [
-    # {'make': "Tesla", 'model': "Model Y", 'year': 2022, 'km': 1000, 'reg': car.regnumber}
-# ]
 cars = {}
 
 
@@ -36,17 +10,15 @@ def opprette_bil():
     regn = input("Registrerings nummer: ")
     drivstoff = input("Drivstoff type: ")
     km = input("Kilometerstand: ")
-    bilObjekt = car(regn, drivstoff, model, make, km, year)
-    kallenavn = year + make + model
+    bilObjekt = Car(regn, drivstoff, model, make, km, year)
+    kallenavn = year + " " + make + " " + model
     cars[kallenavn] = [bilObjekt]
 
 
 opprette_bil()
-# opprette_bil()
-# opprette_bil()
+opprette_bil()
+opprette_bil()
 
 for car in cars:
     print(car)
 
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
