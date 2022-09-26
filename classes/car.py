8848c0558d4e36480eb6a02dc9917b235f5f9452
Ole4961 <marius.torvmark@gmail.com>
2022-09-26 22:33:36 +0200

    La til flere attributs til car klassen

--- classes/car.py
+++ classes/car.py
@@ -1,7 +1,9 @@
 class Car:
-    def __init__(self, regnumber, fuel_source, model, make):
+    def __init__(self, regnumber, fuel_source, model, make, year, km):
         self.regnumber = regnumber
         self.fuel_source = fuel_source
         self.model = model
         self.make = make
+        self.km = km
+        self.year = year
 
