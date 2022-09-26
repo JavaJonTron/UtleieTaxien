c420d241684e0c8cd52660696f447a8522985427
Ole4961 <marius.torvmark@gmail.com>
2022-09-26 14:44:37 +0200

    Tester 1234556788

--- main.py
+++ main.py
@@ -5,13 +5,12 @@
 
 
 def print_hi(name):
-        #Use a breakpoint in the code line below to debug your script.
-    #print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-# commit -test, fra Jon
-#sdasds
-#"sdjoifjsoigj"
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    # Use a breakpoint in the code line below to debug your script.
+    # print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    # commit -test, fra Jon
+    # Press the green button in the gutter to run the script.
+    if __name__ == '__main__':
+        print_hi('PyCharm')
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
