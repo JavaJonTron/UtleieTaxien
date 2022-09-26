e4354d4c5fb987846d3547a929d376469ca4932e
Ole4961 <marius.torvmark@gmail.com>
2022-09-26 21:23:17 +0200

    Endret navn på klassen utleier til klassen owner, og opprettet klassen renter

--- classes/owner.py
+++ classes/owner.py
@@ -1,4 +1,4 @@
-class utleier:
+class owner:
     #I min mening, begrenset hva leietaker trenger å vite om utleier bortsett fra generisk info
     def __init__(self, age, name, cars):
         self.age = age
