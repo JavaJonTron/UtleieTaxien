ab2b48e44f3ee7e7cda679023c1db981b77ce283
JavaJonTron <113911659+JavaJonTron@users.noreply.github.com>
2022-09-26 22:17:36 +0200

    importerer mappa som heter classes til main

--- classes/car.py
+++ classes/car.py
@@ -1,3 +1,4 @@
 class Car:
     def __init__(self, regnumber):
         self.regnumber = regnumber
+
--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from classes import*
+
+
 # This is a sample Python script.
 
 # Press Shift+F10 to execute it or replace it with your code.
