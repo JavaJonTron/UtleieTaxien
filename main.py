b9901dd5a1f3e1ae43e21ebdd65d48fe72862400
Ole4961 <marius.torvmark@gmail.com>
2022-09-26 13:23:54 +0200

    Tester 1234556788

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 # commit -test, fra Jon
 #sdasds
+#"sdjoifjsoigj"
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
