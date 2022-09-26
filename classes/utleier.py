dba83bd28825d8024dfcd33bdf7b4282a4d7fb68
Ole4961 <marius.torvmark@gmail.com>
2022-09-26 21:21:34 +0200

    Laget basen til klassen utleier

---
+++ classes/utleier.py
@@ -0,0 +1,6 @@
+class utleier:
+    #I min mening, begrenset hva leietaker trenger å vite om utleier bortsett fra generisk info
+    def __init__(self, age, name, cars):
+        self.age = age
+        self.name = name
+        self.cars = cars
