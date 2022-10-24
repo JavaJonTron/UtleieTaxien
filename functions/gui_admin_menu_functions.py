7c2a3608e12bee94dfdfc032aa349694dec29a09
Ole4961 <marius.torvmark@gmail.com>
2022-10-24 10:14:37 +0200

    Completed basic admin login function, started on admin control panel, currently does not work.

---
+++ functions/gui_admin_menu_functions.py
@@ -0,0 +1,5 @@
+import dearpygui.dearpygui as dpg
+
+def log_in_accepted():
+    with dpg.window(label="Admin Control Panel", tag="Admin Login", width=600, height=600):
+        dpg.add_text("TEST")
--- functions/gui_main_menu_functions.py
+++ functions/gui_main_menu_functions.py
@@ -1,4 +1,5 @@
 import dearpygui.dearpygui as dpg
+from functions import gui_admin_menu_functions as gui_admin_func
 def test():
     print("TESTER HER")
 
@@ -8,6 +9,8 @@ def UItest():
 def admin_Login():
     with dpg.window(label="Admin Login", tag="Admin Login", width=300, height=200):
         dpg.add_text("Username:")
-        dpg.add_input_text(Label="Username", tag="username")
+        dpg.add_input_text(tag="username")
         dpg.add_text("Password:")
-        dpg.add_input_text(Label="Password", tag="password")
+        dpg.add_input_text(tag="password")
+        dpg.add_button(label="Log in", tag="adminLogInButton", callback=gui_admin_func.log_in_accepted)
+
