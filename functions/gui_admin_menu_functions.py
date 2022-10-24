b7bc5af60b087d518521495ed44cf9a785bb6a8e
Ole4961 <marius.torvmark@gmail.com>
2022-10-24 11:08:13 +0200

    Log in for admin now functions without username and password, just click log in.

--- functions/gui_admin_menu_functions.py
+++ functions/gui_admin_menu_functions.py
@@ -1,5 +1,6 @@
 import dearpygui.dearpygui as dpg
 
 def log_in_accepted():
-    with dpg.window(label="Admin Control Panel", tag="Admin Login", width=600, height=600):
+    with dpg.window(label="Admin Control Panel", tag="Admin Control Panel", width=400, height=400):
+        dpg.hide_item("Admin Login")
         dpg.add_text("TEST")
