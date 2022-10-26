d79422685bf91de91d86e458596eddb8060d9325
JavaJonTron <113911659+JavaJonTron@users.noreply.github.com>
2022-10-26 15:40:44 +0200

    Added comments to my co-workers with suggestions on things we might change. These comments are in norwegian since they are temporary and will be removed shortly

--- classes/human.py
+++ classes/human.py
@@ -1,6 +1,15 @@
 class Human:
-    def __init__(self, age, sex, name, score):
+    def __init__(self, age, sex, name, score):#id Forslag til at hver enkelt person kan ha en ID
         self.age = age
         self.sex = sex
         self.name = name
         self.score = score
+        #self.id = id
+
+
+#Forslag til at hver enkelt person kan ha en ID. Da blir det enklere MTP lagring
+#1) Da kan vi starte med å laste opp alle brukere fra JSON.
+#2) Vi går gjennom dummybrukerne vi skal opprette og sjekker ID,
+# samsvarer ID på dummybrukerne med en bruker som er opprettet oppretter vi ikke dummybrukeren.
+
+#
--- functions/gui_main_menu_functions.py
+++ functions/gui_main_menu_functions.py
@@ -11,6 +11,15 @@ def UItest():
     dpg.hide_item("RENTER")
     print ("TESTEN FUNGERTE")
 
+#Begynnelse på et forsøk for å se om det er mulig å samle felles kodelogikk i en funksjon
+#Eller kansje vi burde bruke en klasse?
+#Det er mye kode som går igjen flere ganger med GUi og det kan kansje bli litt rotete, uryddig eller uoversiktlig?
+def login(tekst_login, width, height):
+    dpg.delete_item(tekst_login)
+    with dpg.window(label=tekst_login, tag=tekst_login, width=width, height=height):
+        dpg.add_button(label="Log in", tag="adminLogInButton", callback=gui_admin_func.log_in_accepted)
+    pass
+
 def admin_Login():
     dpg.delete_item("Admin Login")
     with dpg.window(label="Admin Login", tag="Admin Login", width=300, height=200):
@@ -39,6 +48,10 @@ def renter_Login():
         dpg.add_input_text(tag="Renter username")
         dpg.add_text("Password:")
         dpg.add_input_text(tag="Renter password")
+        #Er det egentlig nødvendig å opprette brukere via input for at programmet skal fungere?
+        #Kan det heller være en bonus?
+        #Sikre at kjernefunksjonaliteten til programmet fungerer først
+        # også implementere brukeropprettelse hvis vi finner det nødvendig
         dpg.add_button(label="Renter Log in", tag="renterLogInButton", callback=gui_renter_func.log_in_accepted)
         dpg.add_button(label="Renter Log in with Google", tag="renterLogInGoogleButton", callback=gui_renter_func.log_in_accepted)
         dpg.add_button(label="Renter Log in with Vipps", tag="renterLogInVippsButton", callback=gui_renter_func.log_in_accepted)
