24b86c591be8a0b752c735dd6f801ae03fad81b6
JavaJonTron <113911659+JavaJonTron@users.noreply.github.com>
2022-11-07 17:59:59 +0100

    Finally saving and loading works.

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from dummyObjects import dummyOwner2
 from dummyObjects import dummyCar1
 from dummyObjects import dummyCar2
 from dummyObjects import dummyCar3
+from FileHandling import file_handler_pickle
 
 owner_list = []
 car_list = []
@@ -12,42 +13,28 @@ renter_list = []
 bookings_list = []
 
 
-def dummy_owner_creation():
-    dummyOwnerList = []
-    dummyOwnerList.append(dummyOwner1)
-    dummyOwnerList.append(dummyOwner2)
-    for dummy in dummyOwnerList:
-        owner = dummy.create_owner()
-        owner_list.append(owner)
-    for obj in owner_list:
-        print(obj)
-    print(owner_list)
+def save_system(file, current_list):
+    filskriving = file_handler_pickle.File_handler_pickle(file, current_list)
+    filskriving.write_method()
+    del filskriving
 
 
-dummy_owner_creation()
+def load_system(file, li):
+    filskriving = file_handler_pickle.File_handler_pickle(file, None)
+    listen = filskriving.read_method()
+    for xx in listen:
+        li.append(xx)
+    del filskriving
 
 
-def dummy_car_creation(owners):
-    car = dummyCar1.create_car(owners[0])
-    car_list.append(car)
-    car = dummyCar2.create_car(owners[0])
-    car_list.append(car)
-    car = dummyCar3.create_car(owners[1])
-    car_list.append(car)
+load_system("owner_file", owner_list)
+load_system("renter_file", renter_list)
+load_system("car_file", car_list)
 
 
-def dummy_renter_creation():
-    dummyRenterList = []
-    dummyRenterList.append(dummyRenter1)
-    dummyRenterList.append(dummyRenter2)
-    for dummy in dummyRenterList:
-        renter = dummy.create_renter()
-        renter_list.append(renter)
-    for obj in renter_list:
-        print(obj)
-    print(renter_list)
 
-
-dummy_renter_creation()
-
-dummy_car_creation(owner_list)
+save_system("owner_file", owner_list)
+# dummy_renter_creation()
+save_system("renter_file", renter_list)
+# dummy_car_creation(owner_list)
+save_system("car_file", car_list)
