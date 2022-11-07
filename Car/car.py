d1b52cf29c81358f3e8d33888bfcdd47ec5b6a1a
JavaJonTron <113911659+JavaJonTron@users.noreply.github.com>
2022-11-07 13:34:42 +0100

    Made changes to car class

--- Car/car.py
+++ Car/car.py
@@ -1,5 +1,5 @@
 class Car:
-    def __init__(self, make, model, year, license_plate, fuel_source, km, is_take, hourly_rate, daily_rate):
+    def __init__(self, make, model, year, license_plate, fuel_source, km, is_take, hourly_rate, daily_rate, owner):
         self.make = make
         self.model = model
         self.year = year
@@ -9,8 +9,9 @@ class Car:
         self.is_take = is_take
         self.hourly_rate = hourly_rate
         self.daily_rate = daily_rate
+        self.owner = owner
 
 
     def __str__(self):
         return str(self.make) + str(self.model) + str(self.year) + str(self.license_plate) + str(self.fuel_source) + \
-               str(self.km) + str(self.is_take) + str(self.hourly_rate) + str(self.daily_rate)
+               str(self.km) + str(self.is_take) + str(self.hourly_rate) + str(self.daily_rate) + str(self.owner)
---
+++ owner/ownerRepository.py
@@ -0,0 +1,10 @@
+from FileHandling import file_handler_pickle
+
+
+class OwnerRepository:
+    def __init__(self,owner):
+        self.owner = owner
+
+
+
+
