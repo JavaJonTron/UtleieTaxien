9bc36115a880e7ff47f17b964b4059d4a42a6260
Ole4961 <marius.torvmark@gmail.com>
2022-11-08 12:25:25 +0100

    Trying to make date system work smoother, currently does not work

--- functions/create_booking_file.py
+++ functions/create_booking_file.py
@@ -3,14 +3,6 @@ from main import renter_list
 from classes import booking
 from main import bookings_list
 
-from_date = 0
-from_day = 0
-from_month = 0
-from_year = 0
-to_date = 0
-to_day = 0
-to_month = 0
-to_year = 0
 
 def caledar_booking(sender, app_data, user_data):
 
@@ -22,13 +14,14 @@ def caledar_booking(sender, app_data, user_data):
 
 
 def dates_to():
+    dict_dates_to = {}
     to_date = (dpg.get_value("to_date"))
     to_day = int(to_date["month_day"])
     to_month = (int(to_date["month"])) + 1
     to_year = (int(to_date["year"])) + 1900
-    dict_dates_to = {}
+    dict_dates_to = {"To_day": to_day, to_month, to_year}
+
     return dict_dates_to
-    pass
 
 
 def dates_from():
@@ -36,11 +29,11 @@ def dates_from():
 
 
 def booking_func(sender, app_data, user_data):
-
+    to_date = dates_to()
     chosen_car = user_data
     renter = renter_list[0]
 
-    car_object = booking.Booking(renter, from_year, from_month, from_day, to_year, to_month, to_day, chosen_car)
-    print(f"Bil objekt: {car_object}")
-    bookings_list.append(car_object)
-    print(f"Booking liste: {bookings_list}")
+    #car_object = booking.Booking(renter, from_year, from_month, from_day, to_year, to_month, to_day, chosen_car)
+    #print(f"Bil objekt: {car_object}")
+    #bookings_list.append(car_object)
+    #print(f"Booking liste: {bookings_list}")
