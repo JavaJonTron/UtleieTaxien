a9f342293248cbea5cc7109677eb87c658502c61
JavaJonTron <113911659+JavaJonTron@users.noreply.github.com>
2022-11-14 16:46:28 +0100

    Reformatted file to remove warning

--- functions/gui_owner_menu_functions.py
+++ functions/gui_owner_menu_functions.py
@@ -3,9 +3,14 @@ from Car import create_car_file
 from main import owner_list
 import atexit
 
+
 def avslutt():
     print("AVSLUTTES")
+
+
 atexit.register(avslutt)
+
+
 def log_in_accepted(sender, app_data, user_data):
     for owner in owner_list:
         owner.is_logged_in = False
@@ -21,6 +26,7 @@ def log_in_accepted(sender, app_data, user_data):
             dpg.add_menu_item(label="Options", callback=options)
         dpg.add_text("TEST")
 
+
 def new_car():
     # owner_list[user_data].is_logged_in = True
     dpg.delete_item("Owner Control Panel")
@@ -34,16 +40,13 @@ def new_car():
         dpg.add_text("TEST")
         dpg.add_input_text(hint="Make:", no_spaces=True, uppercase=True)
         dpg.add_input_text(hint="Model:", no_spaces=True, uppercase=True)
-        dpg.add_input_text(hint="Year:", no_spaces=True, uppercase=True,)
+        dpg.add_input_text(hint="Year:", no_spaces=True, uppercase=True, )
         dpg.add_input_text(hint="License Plate", no_spaces=True, uppercase=True)
         dpg.add_input_text(hint="Fuel Source:", no_spaces=True, uppercase=True)
         dpg.add_input_text(hint="Odometer:", no_spaces=True, uppercase=True)
         dpg.add_input_text(hint="Hourly Rate:", no_spaces=True, uppercase=True)
         dpg.add_input_text(hint="Daily Rate:", no_spaces=True, uppercase=True)
         dpg.add_button(label="Publish")
-
-
-
 
 
 def see_cars():
@@ -57,7 +60,6 @@ def see_cars():
             dpg.add_menu_item(label="See my cars", callback=see_cars)
             dpg.add_menu_item(label="Options", callback=options)
         dpg.add_text("TEST")
-
 
 
 def options():
