b45469c716ab99fc29259ad271bcf23a4fe734f6
JavaJonTron <113911659+JavaJonTron@users.noreply.github.com>
2022-11-28 08:39:38 +0100

    I made a new popup file. where i will store different kinds of popups

---
+++ GUI/Popups.py
@@ -0,0 +1,17 @@
+from dearpygui import dearpygui as dpg
+
+
+def popup_gui_made_a_booking_request():
+    pass
+
+
+def popup_gui_insufficient_funds():
+    pass
+
+
+def popup_gui_to_date_is_smaller_than_from_date():
+    pass
+
+
+def popup_gui_booking_request_dates_crashing_with_previous_booking():
+    pass
