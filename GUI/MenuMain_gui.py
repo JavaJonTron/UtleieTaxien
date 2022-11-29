b296e39d319c5dea5692d8e49b4df3141e917551
Ole4961 <marius.torvmark@gmail.com>
2022-11-29 10:45:42 +0100

    Removed unnecessary buttons, and renamed remaining buttons for log in interface

--- GUI/MenuMain_gui.py
+++ GUI/MenuMain_gui.py
@@ -49,13 +49,8 @@ def admin_login():
     dpg.delete_item("Main menu")
     with dpg.window(label="Admin Login", tag="Admin Login", width=300, height=200):
         dpg.set_primary_window("Admin Login", True)
-        dpg.add_text("Username:")
-        dpg.add_input_text(tag="Admin username")
-        dpg.add_text("Password:")
-        dpg.add_input_text(tag="Admin password")
         dpg.add_button(label="Admin Log in", tag="admin1LogInButton", callback=log_in_accepted,
                        user_data=admin_list[0])
-        dpg.add_button(label="Log in", tag="adminLogInButton", callback=log_in_accepted)
 
 
 def owner_login():
@@ -68,16 +63,10 @@ def owner_login():
     dpg.delete_item("Main menu")
     with dpg.window(label="Owner Login", tag="Owner Login", width=300, height=200):
         dpg.set_primary_window("Owner Login", True)
-        dpg.add_text("Username:")
-        dpg.add_input_text(tag="Owner username")
-        dpg.add_text("Password:")
-        dpg.add_input_text(tag="Owner password")
-        dpg.add_button(label="Owner 1 Log in", tag="owner1LogInButton", callback=log_in_accepted,
+        dpg.add_button(label="Owner 1 (Vipps/Google/Own Log in)", tag="owner1LogInButton", callback=log_in_accepted,
                        user_data=owner_list[0])
-        dpg.add_button(label="Owner 2 Log in", tag="owner2LogInButton", callback=log_in_accepted,
+        dpg.add_button(label="Owner 2 (Vipps/Google/Own Log in)", tag="owner2LogInButton", callback=log_in_accepted,
                        user_data=owner_list[1])
-        dpg.add_button(label="Owner Log in with Google", tag="ownerLogInGoogleButton")
-        dpg.add_button(label="Owner Log in with Vipps", tag="ownerLogInVippsButton")
 
 
 def renter_login():
@@ -91,17 +80,10 @@ def renter_login():
     dpg.delete_item("Main menu")
     with dpg.window(label="Renter Login", tag="Renter Login", width=300, height=200):
         dpg.set_primary_window("Renter Login", True)
-        dpg.add_text("Username:")
-        dpg.add_input_text(tag="Renter username")
-        dpg.add_text("Password:")
-        dpg.add_input_text(tag="Renter password")
-        dpg.add_button(label="Renter 1 Log in", tag="renter1LogInButton", callback=log_in_accepted,
+        dpg.add_button(label="Renter 1 (Vipps/Google/Own Log in)", tag="renter1LogInButton", callback=log_in_accepted,
                        user_data=renter_list[0])
-        dpg.add_button(label="Renter 2 Log in", tag="renter2LogInButton", callback=log_in_accepted,
+        dpg.add_button(label="Renter 2 (Vipps/Google/Own Log in)", tag="renter2LogInButton", callback=log_in_accepted,
                        user_data=renter_list[1])
-        dpg.add_button(label="Renter Log in", tag="renterLogInButton")
-        dpg.add_button(label="Renter Log in with Google", tag="renterLogInGoogleButton")
-        dpg.add_button(label="Renter Log in with Vipps", tag="renterLogInVippsButton")
 
 
 def main_menu():
